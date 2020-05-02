c1776789d4dd93258963e35e06e4dd0ec12dba0b
Diego Pacheco <dipacmon@live.com>
2020-05-01 20:49:50 -0500

    Final

--- servidor.cpp
+++ servidor.cpp
@@ -16,7 +16,7 @@ int main(int argc, char* argv[]) {
     SocketDatagrama  servidor = SocketDatagrama(6000);
     PaqueteDatagrama paqueteRecibido = PaqueteDatagrama(6000);
     cout << "Esperando Respuesta:\n";
-           ofstream fs(argv[2]);
+           ofstream fs(argv[1]);
     while (1) {
         servidor.recibe(paqueteRecibido);
         cout << "Se recibio el voto: " <<paqueteRecibido.obtieneDatos()<<endl;
